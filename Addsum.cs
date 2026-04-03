974057d3efeb6d0c1469bbdb63b8fd15f17d36eb
06Shiva <test@gmail.com>
2026-04-03 14:57:47 +0530

    added sum

---
+++ Addsum.cs
@@ -0,0 +1,27 @@
+﻿namespace programs;
+
+public class Addsum
+{
+    static void Main(string[] args)
+    {
+        int number, sum = 0, r;
+
+        Console.WriteLine("Enter a number for get the sum of digits:");
+
+
+        number = int.Parse(Console.ReadLine()!);
+
+        while (number > 0)
+        {
+            r = number % 10;
+
+            sum = sum + r;
+
+            number = number / 10;
+        }
+
+        Console.WriteLine($"Sum of digits for the given number: {sum}");
+
+    }
+
+}
--- obj/Debug/net8.0/programs.AssemblyInfo.cs
+++ obj/Debug/net8.0/programs.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("programs")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7e6bda3ff27ddfcfefcc969b618eebb4b264908d")]
 [assembly: System.Reflection.AssemblyProductAttribute("programs")]
 [assembly: System.Reflection.AssemblyTitleAttribute("programs")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
